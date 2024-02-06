de40ae325877f25375b069e76b8ced777726d5e3
Hirokazu Chiba <hchiba1@gmail.com>
2024-02-06 16:39:06 +0900

    Add genomes/rdf/assembly_summary_refseq.pl

---
+++ genomes/rdf/assembly_summary_refseq.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+use strict;
+use File::Basename;
+use Getopt::Std;
+my $PROGRAM = basename $0;
+my $USAGE=
+"Usage: $PROGRAM
+";
+
+my %OPT;
+getopts('', \%OPT);
+
+print "\@prefix assemblyId: <https://identifiers.org/assembly/> .\n";
+print "\@prefix ncbio: <https://dbcls.github.io/ncbigene-rdf/ontology.ttl#>";
+print "\@prefix taxid: <http://identifiers.org/taxonomy/> .\n";
+print "\n";
+
+while (<>) {
+    chomp;
+    if (/^#/) {
+        next;
+    }
+    my @f = split(/\t/, $_, -1);
+    if (@f != 38) {
+        die;
+    }
+    my $id = $f[0];
+    my $category = $f[4];
+    my $taxid = $f[5];
+    if ($category eq 'na') {
+        next;
+    }
+    print "assemblyId:$id a ncbio:RefSeqGenome ;\n";
+    print "    ncbio:taxid taxid:$taxid .\n";
+}
