186aa3d976ab603aea83db088c3c52a9c31909f7
Hirokazu Chiba <hchiba1@gmail.com>
2024-02-13 15:14:59 +0900

    Move columns of metadata

--- parser/json.js
+++ parser/json.js
@@ -26,7 +26,6 @@ rl.on('line', (line) => {
     obj.organism.common_name,
     obj.assembly_info.refseq_category,
     obj.assembly_info.assembly_level,
-    obj.assembly_info.assembly_method,
     obj.assembly_stats.genome_coverage,
     obj.assembly_stats.contig_n50,
     obj.annotation_info?.busco?.busco_lineage,
@@ -35,6 +34,7 @@ rl.on('line', (line) => {
     obj.assembly_stats.total_sequence_length,
     obj.assembly_stats.total_number_of_chromosomes,
     obj.assembly_info.sequencing_tech,
+    obj.assembly_info.assembly_method,
     obj.assembly_info.release_date,
     obj.assembly_info.submitter,
   ];
