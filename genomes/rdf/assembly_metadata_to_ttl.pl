79941a096e594fdeeb1a6db60731071c70ee51e8
Hirokazu Chiba <hchiba1@gmail.com>
2024-02-16 15:41:19 +0900

    Modify genomes/rdf/assembly_metadata_to_ttl.pl to add taxid

--- genomes/rdf/assembly_metadata_to_ttl.pl
+++ genomes/rdf/assembly_metadata_to_ttl.pl
@@ -11,6 +11,7 @@ my %OPT;
 getopts('', \%OPT);
 
 print "\@prefix assemblyId: <https://identifiers.org/assembly/> .\n";
+print "\@prefix taxid: <http://identifiers.org/taxonomy/> .\n";
 print "\@prefix ncbio: <https://dbcls.github.io/ncbigene-rdf/ontology.ttl#> .\n";
 print "\n";
 
@@ -18,6 +19,13 @@ while (<>) {
     chomp;
     my @f = split(/\t/, $_, -1);
     my $id = $f[0];
-    s/\\/\\\\/g;
-    print "assemblyId:$id ncbio:metadata '''$_ ''' .\n";
+    my $taxid = $f[1];
+    if ($taxid !~ /^\d+$/) {
+        die;
+    }
+    s/\\/\\\\/g; # escape backslash
+    print "assemblyId:$id\n";
+    print "    ncbio:taxid taxid:$taxid ;\n";
+    print "    ncbio:metadata '''$_ ''' .\n";
+    print "\n";
 }
